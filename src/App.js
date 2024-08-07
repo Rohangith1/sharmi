ea9e65da05a2570246869ce631d345a47c493800
Rohangith1 <107810397+Rohangith1@users.noreply.github.com>
2024-08-07 16:14:27 +0530

    nav done ....

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react'
-import Navbar from './components/Navbar';
+import React from "react";
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home'
+import About from './pages/About'
+import Blog from './pages/Blog'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/contact" element={<Footer />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
-}
+};
 
-export default App
+export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,36 @@
 import React, { useRef } from "react";
 import { FaBarsStaggered } from "react-icons/fa6";
 import { FaTimes } from "react-icons/fa";
-import '../styles/navbar.css'
+import "../styles/navbar.css";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   const navRef = useRef();
   const showNavbar = () => {
     navRef.current.classList.toggle("responsive_nav");
   };
+ const handleLinkClick = () => {
+   if (navRef.current.classList.contains("responsive_nav")) {
+     showNavbar();
+   }
+ };
   return (
     <header>
       <h3>Logo</h3>
       <nav ref={navRef}>
-        <a href="#">Home</a>
-        <a href="#">Blog</a>
-        <a href="#">About Me</a>
-        <a href="#">Contact Me</a>
+        <NavLink className="na" onClick={handleLinkClick} to="/">
+          Home
+        </NavLink>
+        <NavLink className="na" onClick={handleLinkClick} to="/about">
+          About Me
+        </NavLink>
+        <NavLink className="na" onClick={handleLinkClick} to="/blog">
+          Blog
+        </NavLink>
+
+        <NavLink className="na" onClick={handleLinkClick} to="/contact">
+          Contact Me
+        </NavLink>
 
         <button className="nav-btn nav-close-btn" onClick={showNavbar}>
           <FaTimes />
