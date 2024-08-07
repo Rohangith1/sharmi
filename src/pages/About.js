cb9717316f88f9eca030316c90a2ce6e4b12b833
Rohangith1 <107810397+Rohangith1@users.noreply.github.com>
2024-08-08 02:34:46 +0530

    basic done

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import { Fade, Slide } from "react-awesome-reveal";
 
+import { FaSearch } from "react-icons/fa";
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="flex flex-col gap-8 md:flex-col space-y-6 m-4 lg:justify-between lg:flex-col">
+      <Slide>
+        <h1 className=" text-2xl font-bold bg-gradient-to-b from-slate-50 to-pink-100 bg-clip-text text-transparent">
+          About Me
+        </h1>
+      </Slide>
+      <div action="" className="flex flex-col gap-5 md:flex-col rounded-md">
+        <Fade
+          cascade
+          delay={1000}
+          className="flex flex-col gap-5 md:flex-col rounded-md"
+        >
+          <div className=" flex items-center">
+            <p className="text-white font-semibold">
+              Hi, I'm Sharmistha, and I'm currently pursuing a Bachelor of
+              Science (B.Sc.) degree in Physics at Jorhat Institute of Science
+              and Technology.{" "}
+            </p>
+            {/*<input
+              type="email"
+              className="rounded-md px-5 py-4 italic md:px-20 w-full"
+              placeholder="search.."
+              name=""
+              id=""
+            />
+            <FaSearch className="absolute right-4 text-gray-500 cursor-pointer" />*/}
+          </div>
+        </Fade>
+      </div>
+      
+    </div>
+  );
+};
 
-export default About
+export default About;
--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import { Fade, Slide } from "react-awesome-reveal";
 
+import { FaSearch } from "react-icons/fa";
 const Blog = () => {
   return (
-    <div>Blog</div>
-  )
-}
+    <div className="flex flex-col gap-8 md:flex-col lg:flex-col space-y-6 m-4 lg:justify-between">
+      <Slide>
+        <h1 className=" text-2xl font-bold bg-gradient-to-b from-slate-50 to-pink-100 bg-clip-text text-transparent">
+          My blogs
+        </h1>
+      </Slide>
+      <form action="" className="flex flex-col gap-5 md:flex-col rounded-md ">
+        <Fade
+          cascade
+          delay={1000}
+          className="flex flex-col gap-5 md:flex-col rounded-md"
+        >
+          <div className="relative flex items-center">
+            <input
+              type="email"
+              className="rounded-md px-5 py-4 italic md:px-20 w-full"
+              placeholder="search.."
+              name=""
+              id=""
+            />
+            <FaSearch className="absolute right-4 text-gray-500 cursor-pointer" />
+          </div>
+        </Fade>
+      </form>
+      <Slide>
+        <p className="font-semibold bg-gradient-to-b from-slate-50 to-pink-100 bg-clip-text text-transparent">
+          Available soon...
+        </p>
+      </Slide>
+    </div>
+  );
+};
 
-export default Blog
+export default Blog;
